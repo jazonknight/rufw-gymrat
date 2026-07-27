6e7b0e4f5110444e0ca2139fdd5262cc8292606e
Jason Rupert <Jason.Rupert@outlook.com>
2026-07-26 19:22:14 -0500

    update verison in script

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -8,7 +8,7 @@ set -e
 
 DOCKER_USER="${DOCKER_USER:-jazonknight}"
 IMAGE_NAME="rufw-gymrat"
-VERSION="${1:-v2.0.5}"
+VERSION="${1:-v2.0.7}"
 
 echo "📦 [GymRat Docker Publish] Target Registry: ${DOCKER_USER}/${IMAGE_NAME}"
 echo "🏷️  [GymRat Docker Publish] Tagging Version: ${VERSION} and latest"
